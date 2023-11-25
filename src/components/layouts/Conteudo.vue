42cf2366efbd85aefb911a110e7cf6573490d92a
Lucas Alves <lucasalvesnascimento.lan@gmail.com>
2023-11-25 17:08:23 -0300

    Configurando e estilizando página Home

--- src/components/layouts/Conteudo.vue
+++ src/components/layouts/Conteudo.vue
@@ -17,6 +17,7 @@ export default {
         Home,
         PublicarVaga
     }
+    
 }
 </script>
   
--- src/components/views/Home.vue
+++ src/components/views/Home.vue
@@ -1,6 +1,49 @@
 <template>
-    <div>
-        <p>Home</p>
+    <div class="container py-4">
+        <div class="row">
+            <div class="col">
+                <div class="h-100 p-5 bg-light border rounded-3">
+                    <h2>Pesquisar Vagas</h2>
+                    <div class="row">
+                        <div class="col">
+                            <div class="form-group">
+                                <label>Título da vaga</label>
+                                <input type="text" class="form-control" placeholder="Pesquise por palavras-chaves, por exemplo: 'Angular', 'Pleno', 'Vue', 'Analista'">
+                                <small class="form-text text-muted">Informe palavras que estejam relacionadas com o título da vaga que você procura</small>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <button class="btn btn-outline-dark mt-2" type="button">Buscar</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="row mt-5">
+            <div class="col-4">
+                <div class="h-100 p-5 rounded-3 border bg-dark text-white">
+                    <p>Vagas abertas</p>
+                    <h2>18</h2>
+                </div>
+            </div>
+
+            <div class="col-4">
+                <div class="h-100 p-5 rounded-3 border bg-dark text-white">
+                    <p>Profissionais cadastrados</p>
+                    <h2>130</h2>
+                </div>
+            </div>
+
+            <div class="col-4">
+                <div class="h-100 p-5 rounded-3 border bg-dark text-white">
+                    <p>Visitantes online</p>
+                    <h2>10</h2>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
   
