2ed3dae5979c790c5d5457c8d496d79c26182e9a
Lucas Alves <lucasalvesnascimento.lan@gmail.com>
2023-11-25 17:14:16 -0300

    Criação e configuração do componente PesquisarVaga

--- src/components/views/Home.vue
+++ src/components/views/Home.vue
@@ -1,24 +1,8 @@
 <template>
     <div class="container py-4">
         <div class="row">
-            <div class="col">
-                <div class="h-100 p-5 bg-light border rounded-3">
-                    <h2>Pesquisar Vagas</h2>
-                    <div class="row">
-                        <div class="col">
-                            <div class="form-group">
-                                <label>Título da vaga</label>
-                                <input type="text" class="form-control" placeholder="Pesquise por palavras-chaves, por exemplo: 'Angular', 'Pleno', 'Vue', 'Analista'">
-                                <small class="form-text text-muted">Informe palavras que estejam relacionadas com o título da vaga que você procura</small>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col">
-                            <button class="btn btn-outline-dark mt-2" type="button">Buscar</button>
-                        </div>
-                    </div>
-                </div>
+            <div class="col">  
+                <pesquisar-vaga></pesquisar-vaga>
             </div>
         </div>
         
@@ -49,8 +33,13 @@
   
   
 <script>
+import PesquisarVaga from '@/components/comuns/PesquisarVaga.vue';
+
 export default {
-    name: 'Home'
+    name: 'Home',
+    components: {
+        PesquisarVaga
+    }
 }
 </script>
   
