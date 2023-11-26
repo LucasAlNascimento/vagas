106894038d4b60cf9983237ddfe9f7f7ac211cc5
Lucas Alves <lucasalvesnascimento.lan@gmail.com>
2023-11-26 11:37:52 -0300

    Implementando array de objetos de vagas

--- src/components/comuns/Vaga.vue
+++ src/components/comuns/Vaga.vue
@@ -1,19 +1,19 @@
 <template>
     <div class="card">
-        <div class="card-header bg-dark text-white">Título da Vaga</div>
+        <div class="card-header bg-dark text-white">{{titulo}}</div>
         <div class="card-body">
-            <p>Descrição truncada da vaga...</p>
+            <p>{{ descricao }}</p>
         </div>
         <div class="card-footer">
-            <small class="text-muted">Salário: R$ 8.000,00 | Modalidade: Home Office | Tipo: CLT | Publicação:
-                26/11/2023</small>
+            <small class="text-muted">Salário: R$ {{salario}} | Modalidade: {{modalidade}} | Tipo: {{tipo}} | Publicação: {{publicacao}}</small>
         </div>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'Vaga'
+    name: 'Vaga',
+    props: ['titulo', 'descricao', 'salario', 'modalidade', 'tipo', 'publicacao']
 }
 
 </script>
